eb473c3c0a193678ee647fd0420aefd9fa0a4f1e
atharva134kadam <atharvakadam11056@gmail.com>
2026-09-13 21:50:08 +0530

    R

---
+++ constructorOverloading.java
@@ -0,0 +1,29 @@
+class Student{
+    String name;
+    int age;
+    int rollNo;
+    String college;
+
+    Student(String n, int a, int r, String c){
+        name = n;
+        age = a;
+        rollNo = r;
+        college = c;
+    }
+
+    Student(){
+
+    }
+    
+    void printStudent(){
+        System.out.println(name+" , "+age+" , "+rollNo+" , "+college);
+    }
+}
+
+public class constructorOverloading{
+    public static void main(String[] args){
+        Student s1 = new Student("Atharva",19,67,"MIT");
+
+        s1.printStudent();
+    }
+}
