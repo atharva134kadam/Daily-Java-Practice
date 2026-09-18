f4603989beb9adbedcd34a7edfabbafb09c38096
atharva134kadam <atharvakadam11056@gmail.com>
2026-09-18 14:53:47 +0530

    T

--- compilePolymorphism.java
+++ compilePolymorphism.java
@@ -2,23 +2,15 @@ class Animal{
     void run(){
         System.out.println("Animal runs");
     }
-}
-class Duck extends Animal{
-    void run(){
-        System.out.println("Duck runs");
-    }
-}
-class Hen extends Animal{
-    void run(){
-        System.out.println("Hen runs");
+    String run(String animal){
+        System.out.println(animal+" runs");
+        return animal;
     }
 }
 public class compilePolymorphism{
     public static void main(String[] args){
-        Duck d = new Duck();
-        Hen h = new Hen();
-
-        d.run();
-        h.run();
+        Animal a = new Animal();
+        a.run();
+        a.run("Peacock");
     }
 }
