7bbae254ae1cf2c789571efd337773bc781f0b3f
atharva134kadam <atharvakadam11056@gmail.com>
2026-09-20 20:16:35 +0530

    T

---
+++ autoboxingUnboxing.java
@@ -0,0 +1,10 @@
+public class autoboxingUnboxing{
+    public static void main(String[] args){
+        int a=10;
+        Integer b = 90;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
