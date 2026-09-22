2c5b5a508a4a930eb9042646cdb9876542db9a0e
atharva134kadam <atharvakadam11056@gmail.com>
2026-09-22 21:28:24 +0530

    Y

---
+++ multipleInheritance.java
@@ -0,0 +1,25 @@
+interface A{
+    void fun();
+}
+
+interface B extends A{
+    public void fun();
+}
+
+interface C extends A{
+    public void fun();
+}
+
+class D implements A,B{
+    @Override
+    public void fun(){
+        System.out.println("This is fun");
+    }
+}
+
+public class multipleInheritance{
+    public static void main(String[] args){
+        D d = new D();
+        d.fun();
+    }
+}
