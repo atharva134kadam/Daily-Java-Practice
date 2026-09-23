d5af83a3262e37c9642e1077b4a93bebc7799639
atharva134kadam <atharvakadam11056@gmail.com>
2026-09-23 20:06:56 +0530

    Y

---
+++ string1.java
@@ -0,0 +1,16 @@
+public class string1{
+    public static void main(String[] args){
+        String s1="ja"+"va";
+        String s2 = "java";
+        System.out.println(s1==s2);
+
+        String s3 = "java";
+        String s4 = s3;
+        System.out.println(s2==s3);
+
+        String s5 = "Hello";
+        s5 = "world";
+        System.out.println(s5);
+
+    }
+}
